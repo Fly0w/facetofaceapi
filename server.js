01160cf4f7718360b62aaba8162c68f418c132b3
Fly0w <florian.budniewski@hotmail.com>
2023-05-03 18:50:52 +0900

    Updated server.js

--- server.js
+++ server.js
@@ -10,15 +10,7 @@ import { idHandler } from './controllers/id.js';
 import { changeName } from './controllers/myPage.js';
 
 
-  // const db = knex({
-  //   client: 'pg',
-  //   connection: {
-  //     connectionString: "postgres://hkbrbdmoikszbt:8bd77b6fb428ee84429bc90a9813779322900d0230c638960bb75050895631f5@ec2-34-193-110-25.compute-1.amazonaws.com:5432/davlaum7uh7apf",
-  //     ssl: {
-  //       rejectUnauthorized: false
-  //     }
-  //   }
-  // });
+
 
   const db = knex({
     client: 'pg',
