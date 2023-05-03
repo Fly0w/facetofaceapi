c46a61a0d07fcb215aa4b7f2106f9b4bcebd945d
Fly0w <florian.budniewski@hotmail.com>
2023-05-03 18:44:18 +0900

    Updated server.js

--- server.js
+++ server.js
@@ -10,25 +10,25 @@ import { idHandler } from './controllers/id.js';
 import { changeName } from './controllers/myPage.js';
 
 
-  const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString: "postgres://hkbrbdmoikszbt:8bd77b6fb428ee84429bc90a9813779322900d0230c638960bb75050895631f5@ec2-34-193-110-25.compute-1.amazonaws.com:5432/davlaum7uh7apf",
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
-  });
-
   // const db = knex({
   //   client: 'pg',
   //   connection: {
-  //     connectionString: process.env.DATABASE_URL,
+  //     connectionString: "postgres://hkbrbdmoikszbt:8bd77b6fb428ee84429bc90a9813779322900d0230c638960bb75050895631f5@ec2-34-193-110-25.compute-1.amazonaws.com:5432/davlaum7uh7apf",
   //     ssl: {
   //       rejectUnauthorized: false
   //     }
   //   }
   // });
+
+  const db = knex({
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  });
 
 const app = express();
 app.use(express.json());
@@ -54,8 +54,8 @@ app.post('/register', (req,res) => {registerHandler(req, res, bcrypt, db)})
 app.put('/changeName', (req, res) => {changeName(req, res, db)})
 
 
-// const PORT = process.env.PORT;
-// app.listen(PORT, () => {console.log(`app is running on port ${PORT}`);})
-
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {console.log(`app is running on port ${PORT}`);})
+
+// const PORT = 3001;
+// app.listen(PORT, () => {console.log(`app is running on port ${PORT}`);})
